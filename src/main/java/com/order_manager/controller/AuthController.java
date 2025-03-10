1b0b795b3b09e6610307399709be5d599c098712
Владислав <vladstorm98@gmail.com>
2025-03-10 23:23:10 +0200

    Added Swagger UI in project

--- src/main/java/com/order_manager/controller/AuthController.java
+++ src/main/java/com/order_manager/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.order_manager.dto.AuthResponse;
 import com.order_manager.entity.UserRole;
 import com.order_manager.security.JwtTokenProvider;
 import com.order_manager.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Authentication")
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
@@ -23,11 +26,13 @@ public class AuthController {
     private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/register")
+    @Operation(summary = "Registration a new user")
     public void register(@RequestBody @Valid AuthRequest request) {
         authService.registerUser(request.getUsername(), request.getPassword(), UserRole.USER);
     }
 
     @PostMapping("/login")
+    @Operation(summary = "User authorization")
     public AuthResponse login(@RequestBody AuthRequest request) {
 
         Authentication authentication = authenticationManager.authenticate(
@@ -40,6 +45,7 @@ public class AuthController {
     }
 
     @PostMapping("/logout")
+    @Operation(summary = "User logout")
     public void logout() {
         SecurityContextHolder.clearContext();
     }
--- src/main/java/com/order_manager/controller/ProductController.java
+++ src/main/java/com/order_manager/controller/ProductController.java
@@ -3,12 +3,15 @@ package com.order_manager.controller;
 import com.order_manager.dto.ProductRequest;
 import com.order_manager.dto.ProductResponse;
 import com.order_manager.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Tag(name = "Products")
 @RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
@@ -16,21 +19,25 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
+    @Operation(summary = "Get the list of all product")
     public List<ProductResponse> getProducts() {
         return productService.getAllProducts();
     }
 
     @PostMapping
+    @Operation(summary = "Add a new product")
     public ProductResponse addProduct(@RequestBody ProductRequest request) {
         return productService.createProduct(request);
     }
 
     @PutMapping("/{productId}")
+    @Operation(summary = "Update information about product by ID")
     public ProductResponse updateProduct(@PathVariable Long productId, @RequestBody ProductRequest request) {
         return productService.updateProduct(productId, request);
     }
 
     @DeleteMapping("/{productId}")
+    @Operation(summary = "Delete the product by ID")
     public void deleteProduct(@PathVariable Long productId) {
         productService.deleteProduct(productId);
     }

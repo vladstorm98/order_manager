2ebf8d31725b47e4f5ea9b311a4f1635d3c2c9d9
Владислав <vladstorm98@gmail.com>
2025-03-28 14:51:31 +0200

    Removal duplicate calls to Testcontainers

--- src/test/java/com/order_manager/BaseTest.java
+++ src/test/java/com/order_manager/BaseTest.java
@@ -4,27 +4,14 @@ import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class BaseTest {
 
-    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17.4");
-
     @Autowired
     private Flyway flyway;
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        postgres.start();
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 
     @BeforeEach
     void cleanDatabase() {
